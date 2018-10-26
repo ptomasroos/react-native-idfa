71efca38a09ea8fe677b5579731c2f37983a2f40
Tomas Roos <ptomasroos@gmail.com>
2018-10-26 07:41:28 +0200

    Added back working working examples/Basic based on latest rn release

---
+++ examples/Basic/App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { IDFA } from 'react-native-idfa';
+
+type Props = {};
+export default class App extends Component<Props> {
+  state = {
+    IDFA: '',
+  };
+
+  componentDidMount() {
+    IDFA.getIDFA()
+      .then(idfa => {
+        this.setState({ IDFA: idfa });
+      })
+      .catch(e => {
+        console.error(e);
+      });
+  }
+
+  render() {
+    return (
+      <View
+        style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}
+      >
+        <Text style={{ textAlign: 'center' }}>
+          Your IDFA is : {this.state.IDFA}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
