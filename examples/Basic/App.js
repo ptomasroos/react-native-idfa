460f44380cee82376b40e0c151d083f8ea4534e9
Tomas Roos <ptomasroos@gmail.com>
2018-11-07 12:33:06 +0100

    Updated App.js implementation to support test

--- examples/Basic/App.js
+++ examples/Basic/App.js
@@ -31,9 +31,12 @@ export default class App extends Component<Props> {
       <View
         style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}
       >
-        <Text style={{ textAlign: 'center' }}>
-          Your IDFA is : {this.state.IDFA}
-        </Text>
+        <Text style={{ textAlign: 'center' }}>Your IDFA is :</Text>
+        {this.state.IDFA !== '' && (
+          <Text style={{ textAlign: 'center' }} testID="idfa">
+            {this.state.IDFA}
+          </Text>
+        )}
       </View>
     );
   }
