f6ab1bbfade87918857c440a3132adb40afe4ad0
Писарев Кирилл <k.pisarev@maximatelecom.ru>
2020-07-27 13:59:36 +0300

    chore(ios): fix requestTrackingPermission android stub

--- IDFA.js
+++ IDFA.js
@@ -7,12 +7,12 @@ const { PTRIDFA } = NativeModules;
 class IDFA {
   getIDFA(): Promise<string> {
     return PTRIDFA.getIDFA();
-  },
+  }
   requestTrackingPermission(): Promise<string> {
     if (Platform.OS === "ios")
       return PTRIDFA.requestTrackingPermission();
     else
-      return Promise.resolve();
+      return Promise.resolve("authorized");
   }
 }
 
